07f409187863ef635258325c44395fd2c9670246
Rahul Raya <76113996+rahulraya101@users.noreply.github.com>
2023-02-15 16:14:16 +0100

    Update cpu_usage.py

--- cpu_usage.py
+++ cpu_usage.py
@@ -3,3 +3,10 @@ import psutil
 
 # Calling psutil.cpu_precent() for 4 seconds
 print('The CPU usage is: ', psutil.cpu_percent(4))
+
+
+
+
+
+source
+https://www.geeksforgeeks.org/how-to-get-current-cpu-and-ram-usage-in-python/
